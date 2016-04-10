8768d5c236810c78a4a680058e8518c1c6c79289
Nishant Gupta <ng.nishant@gmail.com>
2016-04-10 19:16:25 -0400

    bowler changes..

--- DreamTeamCreator/AnonUsers/Bowlers.aspx
+++ DreamTeamCreator/AnonUsers/Bowlers.aspx
@@ -1,3 +1,15 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Bowlers.aspx.cs" Inherits="DreamTeamCreator.AnonUsers.Bowlers" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Bowlers.aspx.cs" Inherits="DreamTeamCreator.AnonUsers.Bowlers"%>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+<br></br>
+<br></br>
+<table style="width: 100%;">
+<tr>
+    <td>
+        <asp:DropDownList ID="DropTeam" runat="server" OnSelectedIndexChanged="DropTeamSelect" DataTextField = "teams" DataValueField = "id"></asp:DropDownList>
+    </td>
+    <td>
+        <asp:TextBox ID="Name" runat="server" placeholder="Bowler Name"></asp:TextBox>
+    </td>
+</tr>
+</table>
 </asp:Content>
--- DreamTeamCreator/AnonUsers/Bowlers.aspx.cs
+++ DreamTeamCreator/AnonUsers/Bowlers.aspx.cs
@@ -4,12 +4,41 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
+using System.Configuration;
+using System.Data;
+using System.Web.Security;
 
 namespace DreamTeamCreator.AnonUsers
 {
     public partial class Bowlers : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
+        {
+            string oracleConnectionString = ConfigurationManager.ConnectionStrings["OracleConnection"].ConnectionString;
+            OracleConnection con = new OracleConnection(oracleConnectionString);
+            if (!IsPostBack)
+            {
+                try
+                {
+                    OracleCommand cmd = new OracleCommand("select 0 as id,'Select Team..' teams from dual union all select distinct team_id id, teams from team_ids", con);
+                    con.Open();
+                    DropTeam.DataSource = cmd.ExecuteReader();
+                    DropTeam.DataBind();
+                }
+                catch (Exception ex)
+                {
+                    Response.Write("<br/><br/><br/><br/><br/>" + ex.Message);
+                }
+                finally
+                {
+                    con.Close();
+                }
+            }            
+        }
+
+        protected void DropTeamSelect(object sender, EventArgs e)
         {
 
         }
