99e6bd331c2c3d7c258e1179e08359a074c7043b
Nishant Gupta <ng.nishant@gmail.com>
2016-04-15 20:07:29 -0400

    Ran query and tested filters

--- DreamTeamCreator/AnonUsers/Bowlers.aspx.cs
+++ DreamTeamCreator/AnonUsers/Bowlers.aspx.cs
@@ -78,33 +78,41 @@ namespace DreamTeamCreator.AnonUsers
 
         protected void Search_Click(object sender, EventArgs e)
         {
-            string[] conditions=null;int i = 0;
-            string master_q="",search_q = "",econ_f="",wickets_f="",player_f=""; //data filtering conditions
-            master_q = "select * from (select distinct season,bowler,sum(wickets_taken) wickets_taken,sum(economy) economy from (select distinct season, bowler, 0 as wickets_taken, cast((sum(run_scored) / (count(*) / 6)) as numeric(15, 2)) as economy from ball_by_ball where season <> '2015' group by season, bowler union all select distinct season, bowler, count(*) as wickets_taken, 0 as economy from ball_by_ball where out_decision <> '*' and season <> '2015' group by season, bowler) A group by A.season,A.bowler) all";
+            List<String> conditions=new List<string>();
+            //int i = 0;
+            //conditions.Add("");
+
+            string master_q = "";
+            string search_q = "";
+            string econ_f = "";
+            string wickets_f = "";
+            string player_f=""; //data filtering conditions
+
+            master_q = "select * from (select distinct season,bowler,sum(wickets_taken) wickets_taken,sum(economy) economy from (select distinct season, bowler, 0 as wickets_taken, cast((sum(run_scored) / (count(*) / 6)) as numeric(15, 2)) as economy from ball_by_ball where season <> '2015' group by season, bowler union all select distinct season, bowler, count(*) as wickets_taken, 0 as economy from ball_by_ball where out_decision <> '*' and season <> '2015' group by season, bowler) A group by A.season,A.bowler) all_data";
 
             if (!string.IsNullOrWhiteSpace(Name.Text)){
                 player_f = "bowler='" + Name.Text + "' ";
-                conditions[i++] = player_f;
+                conditions.Add(player_f);
             }
 
             if (!string.IsNullOrEmpty(econrange)) {
                 //int high,low;
-                int[] range = split_input_range(econrange);
-                if (range.Length == 1) {
+                List<int> range = split_input_range(econrange);
+                if (range.ToArray().Length == 1) {
                     econ_f = "economy "+econrange+" ";
                 }
                 else
                 {
                     econ_f = "economy >=" + range[0] + " and economy < " + range[1] + " ";
                 }
-                conditions[i++] = econ_f;
+                conditions.Add(econ_f);
             }
 
             if (!string.IsNullOrEmpty(wicketsrange))
             {
                 //int high,low;
-                int[] range = split_input_range(wicketsrange);
-                if (range.Length == 1)
+                List<int> range = split_input_range(wicketsrange);
+                if (range.ToArray().Length == 1)
                 {
                     wickets_f = "wickets_taken " + wicketsrange + " ";
                 }
@@ -112,11 +120,11 @@ namespace DreamTeamCreator.AnonUsers
                 {
                     wickets_f = "wickets_taken >=" + range[0] + " and wickets_taken < " + range[1] + " ";
                 }
-                conditions[i++] = wickets_f;
+                conditions.Add(wickets_f);
             }
 
             search_q = master_q;
-            for(int k=0; k < conditions.Length; k++)
+            for(int k=0; k < conditions.ToArray().Length; k++)
             {
                 if (k == 0)
                 {
@@ -126,22 +134,39 @@ namespace DreamTeamCreator.AnonUsers
                 search_q += " and " + conditions[k];
             }
 
+            string oracleConnectionString = ConfigurationManager.ConnectionStrings["OracleConnection"].ConnectionString;
+            OracleConnection con = new OracleConnection(oracleConnectionString);
+            try
+            {
+                OracleCommand cmd = new OracleCommand(search_q, con);
+                con.Open();
+                OracleDataReader rdr = cmd.ExecuteReader();
+                BowlerSearchRes.DataSource = rdr;
+                BowlerSearchRes.DataBind();
+            }
+            catch (OracleException ex)
+            {
+                Response.Write("<br>/" + "<br>/" + "<br>/" + "<br>/" + "<br>/" + ex);
+            }
+            finally
+            {
+                con.Close();
+            }
 
-            
         }
 
         //if return of below funtion is single digit, check the 1st character of input string to see if its the lower limit or the upper limit
-        private int[] split_input_range(string input) {
-            int[] range = null;
+        private List<int> split_input_range(string input) {
+            List<int> range = new List<int>();
             string[] range_parse= input.Split(' ');
-            if (range.Length > 1)
+            if (range_parse.Length> 1)
             {
-                range[0]= Int32.Parse(Regex.Match(range_parse[0], @"\d+").Value);
-                range[1]= Int32.Parse(Regex.Match(range_parse[1], @"\d+").Value);
+                range.Add(Int32.Parse(Regex.Match(range_parse[0], @"\d+").Value));
+                range.Add(Int32.Parse(Regex.Match(range_parse[2], @"\d+").Value));
             }
             else
             {
-                range[0]= Int32.Parse(Regex.Match(range_parse[0], @"\d+").Value);
+                range.Add(Int32.Parse(Regex.Match(range_parse[0], @"\d+").Value));
             }
             return range;
         }
